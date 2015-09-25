7e3fb14038ea0522fc9302b9a2b6be60551bde6d
Brendon Brewer <brendon.brewer@gmail.com>
2015-09-25 14:11:34 +0100

    Some methods (load not finished!!)

--- PSF.cpp
+++ PSF.cpp
@@ -1,4 +1,6 @@
 #include "PSF.h"
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
@@ -7,5 +9,44 @@ PSF::PSF(int M, int N)
 ,pixels(M, vector<double>(N))
 {
 
+}
+
+void PSF::normalise()
+{
+	double tot = 0.;
+	for(int i=0; i<M; i++)
+		for(int j=0; j<N; j++)
+			tot += pixels[i][j];
+
+	for(int i=0; i<M; i++)
+		for(int j=0; j<N; j++)
+			pixels[i][j] /= tot;
+}
+
+void PSF::load(const char* filename)
+{
+	// Open the file
+	fstream fin(filename, ios::in);
+	if(!fin)
+	{
+		cerr<<"# Error: could not open file "<<filename<<"."<<endl;
+		return;
+	}
+
+	// Read everything
+	double temp;
+	vector<double> everything;
+	while(fin>>temp)
+		everything.push_back(temp);
+
+	// Check the number of values read in vs the supposed size of this
+	// PSF
+	if(static_cast<int>(everything.size()) != M*N)
+	{
+		cerr<<"# Error: wrong number of elements in file ";
+		cerr<<filename<<"."<<endl;
+		cerr<<"# Expected "<<(M*N)<<", found "<<everything.size()<<"."<<endl;
+		return;
+	}
 }
 
--- PSF.h
+++ PSF.h
@@ -14,6 +14,12 @@ class PSF
 		// Constructor inputs specify the size of the
 		// psf in pixels (vertical then horizontal)
 		PSF(int M, int N);
+
+		// Load from file (in centered format)
+		void load(const char* filename);
+
+		// Normalise so that the sum is 1
+		void normalise();
 };
 
 #endif
